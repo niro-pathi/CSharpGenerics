88aaf3dd27f92b581ca91058ce617797c2fe9830
Capt. Niro <niroshana.pathirana@crownresorts.com.au>
2021-06-20 15:29:26 +1000

    first commit

---
+++ src/Generics/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackDouble();
+            StackString();
+        }
+
+        private static void StackDouble()
+        {
+            var stack = new SimpleStack<double>();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            double sum = 0.0;
+
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item : {item}");
+                sum += item;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+
+        private static void StackString()
+        {
+            var stack = new SimpleStack<string>();
+            stack.Push("Pluralsight");
+            stack.Push("Target");
+            stack.Push("Nike");
+
+            while (stack.Count > 0)
+            {
+                string item = stack.Pop();
+                Console.WriteLine($"Item : {item}");
+            }
+        }
+    }
+}
---
+++ test/Generics.Tests/StackTests.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+
+namespace Generics.Tests
+{
+    public class StackTests
+    {
+        [Fact]
+        public void DoubleTypeStack()
+        {
+            var stack = new SimpleStack<double>();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            Assert.Equal(2, stack.Count);
+            Assert.Equal(2.8, stack.Pop(),1);
+            Assert.Equal(1.2, stack.Pop(),1);
+        }
+
+        [Fact]
+        public void StringTypeStack()
+        {
+            var stack = new SimpleStack<string>();
+            stack.Push("Test 01");
+            stack.Push("Test 02");
+            stack.Push("Test 03");
+            Assert.Equal(3, stack.Count);
+            Assert.Equal("Test 03", stack.Pop());
+            Assert.Equal("Test 02", stack.Pop());
+            Assert.Equal("Test 01", stack.Pop());
+        }
+
+    }
+}
